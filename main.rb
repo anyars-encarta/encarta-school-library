19ff804d927599ae68b5c27f28762b20c5be280f
Anyars Yussif <anyarsencarta@gmail.com>
2023-10-27 17:12:39 +0000

    Refactored main.rb file

--- main.rb
+++ main.rb
@@ -4,13 +4,13 @@ class Main
   def initialize
     @app = App.new
     @choices = {
-      1 => method(:list_all_books),
-      2 => method(:list_all_people),
-      3 => method(:create_new_person),
-      4 => method(:create_new_book),
-      5 => method(:create_new_rental),
-      6 => method(:list_rentals_for_person),
-      7 => method(:quit)
+      1 => -> { @app.list_all_books },
+      2 => -> { @app.list_all_people },
+      3 => -> { @app.create_person },
+      4 => -> { @app.create_book },
+      5 => -> { @app.create_rental },
+      6 => -> { @app.list_rentals_for_person },
+      7 => -> { quit }
     }
   end
 
@@ -45,30 +45,6 @@ class Main
     else
       invalid_choice
     end
-  end
-
-  def list_all_books
-    @app.list_all_books
-  end
-
-  def list_all_people
-    @app.list_all_people
-  end
-
-  def create_new_person
-    @app.create_person
-  end
-
-  def create_new_book
-    @app.create_book
-  end
-
-  def create_new_rental
-    @app.create_rental
-  end
-
-  def list_rentals_for_person
-    @app.list_rentals_for_person
   end
 
   def quit
