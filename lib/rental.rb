4dacc50b76c55871067ae603f9a111d8011090f6
Anyars Yussif <anyarsencarta@gmail.com>
2023-10-31 12:03:47 +0000

    Implement to_json and from_json in rental.rb

--- lib/rental.rb
+++ lib/rental.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Rental
   attr_accessor :date, :book, :person
 
@@ -8,5 +10,21 @@ class Rental
 
     book.rentals << self
     person.rentals << self
+  end
+
+  def to_json(*args)
+    {
+      date: @date,
+      book: @book.to_json,
+      person: @person.to_json
+    }.to_json(*args)
+  end
+
+  def self.from_json(json)
+    data = JSON.parse(json)
+    book = Book.from_json(data['book'])
+    person = Person.from_json(data['person'])
+    rental = Rental.new(data['date'], book, person)
+    rental
   end
 end
