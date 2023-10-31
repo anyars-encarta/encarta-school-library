5b546c119fd02a99e81be34bd20bcea91b5bf5ae
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-10-31 20:16:30 +0200

    data load

--- lib/preserve_data/load_books.rb
+++ lib/preserve_data/load_books.rb
@@ -1,11 +1,11 @@
 DATA_PATH = './data'.freeze
 
-# def load_books
-#   file_path = "#{DATA_PATH}/books.json"
-#   nil unless File.exist?(file_path)
+def load_books
+  file_path = "#{DATA_PATH}/books.json"
+  nil unless File.exist?(file_path)
 
-#   json_data = File.read(file_path)
-#   return if json_data.empty?
+  json_data = File.read(file_path)
+  return if json_data.empty?
 
-#   @books = JSON.parse(json_data).map { |data| Book.new(data['title'], data['author']) }
-# end
+  @books = JSON.parse(json_data).map { |data| Book.new(data['title'], data['author']) }
+end
